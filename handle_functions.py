2f86e75e48e8dd9c4631028be50a48eb25d37c0a
dionbalerr <dionbalerr@gmail.com>
2025-09-21 19:03:50 +0800

    Updated error checking in handle_functions.py

--- handle_functions.py
+++ handle_functions.py
@@ -1,186 +1,237 @@
 import pandas as pd
-import math
+import os
+
+df_empty = pd.DataFrame()
+
+
+def file_exist(filename):
+    if os.path.exists(f"./data/{filename}.xlsx"):
+        print(f"File exists in './data/{filename}.xlsx'")
+        return True
+    else:
+        print(f"File does not exist in './data/{filename}.xlsx'")
+        return False
+
 
 def table_sort(filename):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
-    # print(df)
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
 
-    # Tried to chang nan to None, to no avail
-    df = df.where(pd.notnull(df), None)
-    # print(df)
+        # Tried to chang nan to None, to no avail
+        df = df.where(pd.notnull(df), None)
+        # print(df)
 
-    first_col = list(df.columns)[0]
-    # print(list(df.columns)[0])
+        first_col = list(df.columns)[0]
+        # print(list(df.columns)[0])
 
-    df = df.sort_values(first_col)
-    # print(df)
+        df = df.sort_values(first_col)
+        # print(df)
 
-    df.to_excel(f"./data/{filename}.xlsx", index=False)
-    return df.reset_index(drop=True)
+        df.to_excel(f"./data/{filename}.xlsx", index=False)
+        return df.reset_index(drop=True)
+    else:
+        print("Error:", FileNotFoundError)
+        return df_empty
+
 
 # table_sort('testdsave')
 
 def header_val(df):
-    headers = list(df.columns)
-    # print(headers)
-    return headers
+    # check if cols & rows are empty
+    if df.shape != (0, 0):
+        headers = list(df.columns)
+        if len(headers) == 0:
+            print("Error: Empty dataframe")
+        # print(headers)
+        return headers
+    else:
+        print("Error: Empty dataframe")
+        return []
+
 
 def row_values(filename):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
-    rows = df.values
-    # print(rows)
-    return rows
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
+        rows = df.values
+        # print(rows)
+        return rows
+    else:
+        print("Error:", FileNotFoundError)
+        return df_empty
+
 
 # row_values('testdelisite')
 
-testlist = {'DRIVER_NO': ['20'],  'IC_NO': [''], 'LORRY_NO': [''], 'COMISSION': [''], 'BASIC': [''], 'ROAD_TAX': [''], 'INSPECTION': ['']}
+testlist = {'DRIVER_NO': ['20'], 'IC_NO': [''], 'LORRY_NO': [''], 'COMISSION': [''], 'BASIC': [''], 'ROAD_TAX': [''],
+            'INSPECTION': ['']}
 testlist2 = {}
+
+
 def add(add_list, filename):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
 
-    # Convert val to string then uppercase, back into dict
-    add_list = { key: [str(val[0]).upper()] for key, val in add_list.items()}
-    # print("New: ", add_list)
+        # Convert val to string then uppercase, back into dict
+        add_list = {key: [str(val[0]).upper()] for key, val in add_list.items()}
+        # print("New: ", add_list)
 
-    to_add_df = pd.DataFrame(add_list)
-    add_first_col = list(to_add_df.columns)[0]
-    to_add_val = str(to_add_df[add_first_col].values[0]).upper()
+        to_add_df = pd.DataFrame(add_list)
+        add_first_col = list(to_add_df.columns)[0]
+        to_add_val = str(to_add_df[add_first_col].values[0]).upper()
 
-    print(to_add_df)
-    print("Added: ", to_add_val)
+        print(to_add_df)
+        print("Added: ", to_add_val)
 
-    first_col = list(df.columns)[0]
-    # print(first_col)
-    ori_col = df[first_col]
+        first_col = list(df.columns)[0]
+        # print(first_col)
+        ori_col = df[first_col]
 
-    frames = [df, to_add_df]
+        frames = [df, to_add_df]
 
-    # Check if first_col: DRIVER_NO/DELISITE etc unique
-    if df[ori_col == to_add_val].empty:
-        print(f"Unique entry, adding new {first_col}: {to_add_val}...")
-        data = pd.concat(frames)
-        data.to_excel(f"./data/{filename}.xlsx", index=False)
-        print("Data saved")
-        table_sort(filename)
-        flag = True
+        # Check if first_col: DRIVER_NO/DELISITE etc unique
+        if df[ori_col == to_add_val].empty:
+            print(f"Unique entry, adding new {first_col}: {to_add_val}...")
+            data = pd.concat(frames)
+            data.to_excel(f"./data/{filename}.xlsx", index=False)
+            print("Data saved")
+            table_sort(filename)
+            flag = True
+        else:
+            print(f"Duplicated entry, {first_col}: {to_add_val} already exists")
+            data = df
+            flag = False
+
+        print("Data&Flag: ", [data, flag])
+        return [data, flag]
     else:
-        print(f"Duplicated entry, {first_col}: {to_add_val} already exists")
-        data = df
-        flag = False
-
-    print("Data&Flag: ", [data, flag])
-    return [data, flag]
+        print("Error:", FileNotFoundError)
+        return [df_empty, -1]
 
 # add(testlist2, 'testdsave')
 # header_val(table_sort("testdriver"))
 
-testa = {'entry': ["1", "3", "4"]}
+# testa = {'entry': ["1", "3", "4"]}
+
+
 def delete_entry(del_list, filename):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
 
-    if del_list != {}:
-        # Delete tables based on index, which starts from 0
-        new_list = [int(i) - 1 for i in del_list['entry']]
-        # print(new_list)
+        if del_list != {}:
+            # Delete tables based on index, which starts from 0
+            new_list = [int(i) - 1 for i in del_list['entry']]
+            # print(new_list)
 
-        data = df.drop(new_list)
-        # print(data)
-        data.to_excel(f"./data/{filename}.xlsx", index=False)
-        flag = True
+            data = df.drop(new_list)
+            # print(data)
+            data.to_excel(f"./data/{filename}.xlsx", index=False)
+            flag = True
+        else:
+            data = df
+            flag = False
+
+        return [data, flag]
     else:
-        data = df
-        flag = False
+        print("Error:", FileNotFoundError)
+        return [df_empty, -1]
 
-    return [data, flag]
 
 def get_entry(get_list, filename):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
 
-    if get_list != {}:
-        index_list = [int(get_list['radio_entry'][0]) - 1]
-        # print(index_list)
+        if get_list != {}:
+            index_list = [int(get_list['radio_entry'][0]) - 1]
+            # print(index_list)
 
-        # Replace nan with empty str
-        specific_row = df.iloc[index_list[0]]
-        specific_row = specific_row.where(pd.notnull(specific_row), '')
-        # print("Row: ", specific_row)
+            # Replace nan with empty str
+            specific_row = df.iloc[index_list[0]]
+            specific_row = specific_row.where(pd.notnull(specific_row), '')
+            # print("Row: ", specific_row)
 
-        # Row to dict
-        row_dict = specific_row.to_dict()
-        # print(row_dict)
-        return [row_dict, index_list[0]]
+            # Row to dict
+            row_dict = specific_row.to_dict()
+            # print(row_dict)
+            return [row_dict, index_list[0]]
+        else:
+            return [df_empty, -1]
     else:
-        return [{}, -1]
+        print("Error:", FileNotFoundError)
+        return [df_empty, -1]
 
 # get_entry({'radio_entry': ['2']}, 'DRIVER')
 
 # editedit = {'DRIVER_NO': 'A00', 'NAME': "ENGSUNAI E'PRISE  QKA5183", 'IC_NO': '690728135989', 'LORRY_NO': 'QKA5183', 'COMISSION': nan, 'BASIC': nan, 'ROAD_TAX': nan, 'INSPECTION': nan}
+
+
 def edit(edit_list, filename, row_index):
-    df = pd.read_excel(f"./data/{filename}.xlsx")
+    if file_exist(filename):
+        df = pd.read_excel(f"./data/{filename}.xlsx")
 
-    new_list = { key: str(val[0]).upper() for key, val in edit_list.items() }
+        new_list = {key: str(val[0]).upper() for key, val in edit_list.items()}
 
-    print("new edit list: ", new_list)
+        print("new edit list: ", new_list)
 
-    to_edit_df = pd.DataFrame([new_list])
-    edit_first_col = list(to_edit_df.columns)[0]
-    # print(to_edit_df)
+        to_edit_df = pd.DataFrame([new_list])
+        edit_first_col = list(to_edit_df.columns)[0]
+        # print(to_edit_df)
 
-    first_col = list(df.columns)[0]
-    # print(first_col)
+        first_col = list(df.columns)[0]
+        # print(first_col)
 
-    ori_list_1 = df.loc[row_index].to_dict()
-    print("ori1 row: ", ori_list_1)
-    ori_list = { key: [str(val).upper().replace('NAN','')] for key, val in ori_list_1.items() }
-    print("ori row: ", ori_list)
+        ori_list_1 = df.loc[row_index].to_dict()
+        print("ori1 row: ", ori_list_1)
+        ori_list = {key: [str(val).upper().replace('NAN', '')] for key, val in ori_list_1.items()}
+        print("ori row: ", ori_list)
 
-    print("before: ", df.at[row_index, first_col])
-    print("editdf", to_edit_df.at[0, edit_first_col])
+        print("before: ", df.at[row_index, first_col])
+        print("editdf", to_edit_df.at[0, edit_first_col])
 
-    # Check if first_col: DRIVER_NO/DELISITE etc is same
-    if df.at[row_index, first_col] == to_edit_df.at[0, edit_first_col]:
-        # Replace ori row with edit row
-        print("same first col")
-        df.loc[row_index] = new_list
-        print("after replace: ", df.loc[row_index])
-        df.to_excel(f"./data/{filename}.xlsx", index=False)
-        print("Data saved")
-        table_sort(filename)
-        flag = 3
-        return [df, flag]
+        # Check if first_col: DRIVER_NO/DELISITE etc is same
+        if df.at[row_index, first_col] == to_edit_df.at[0, edit_first_col]:
+            # Replace ori row with edit row
+            print("same first col")
+            df.loc[row_index] = new_list
+            print("after replace: ", df.loc[row_index])
+            df.to_excel(f"./data/{filename}.xlsx", index=False)
+            print("Data saved")
+            table_sort(filename)
+            flag = 3
+            return [df, flag]
 
-    else:
-        print("diff first col")
-
-        # Delete ori_col, use add for validation, add ori_col back again
-        data = df.drop(row_index)
-        # print("DEleted:", data)
-        data.to_excel(f"./data/{filename}.xlsx", index=False)
-
-        # Check whole table first_col for dupe
-        data = add(edit_list, filename)
-        # print("Done add: ", data[0])
-        new_dupe_flag = data[1]
-        # print("New edit flag: ", new_dupe_flag)
-
-        # 6 means editted, 7 means edit val dupe
-        if new_dupe_flag:
-            return_flag = 6
         else:
-            return_flag = 7
+            print("diff first col")
 
-        # if New value dupe, means deleted ori_val need to be added back
-        if return_flag == 7:
-            data = add(ori_list, filename)
-            # print("Add ori: ", data[0])
-            ori_dupe_flag = data[1]
-            # print("Ori flag: ", ori_dupe_flag)
+            # Delete ori_col, use add for validation, add ori_col back again
+            data = df.drop(row_index)
+            # print("DEleted:", data)
+            data.to_excel(f"./data/{filename}.xlsx", index=False)
 
-        sorted_df = table_sort(filename)
-        print(sorted_df)
+            # Check whole table first_col for dupe
+            data = add(edit_list, filename)
+            # print("Done add: ", data[0])
+            new_dupe_flag = data[1]
+            # print("New edit flag: ", new_dupe_flag)
 
-        return[sorted_df, return_flag]
+            # 6 means editted, 7 means edit val dupe
+            if new_dupe_flag:
+                return_flag = 6
+            else:
+                return_flag = 7
 
+            # if New value dupe, means deleted ori_val need to be added back
+            if return_flag == 7:
+                data = add(ori_list, filename)
+                # print("Add ori: ", data[0])
+                ori_dupe_flag = data[1]
+                # print("Ori flag: ", ori_dupe_flag)
+
+            sorted_df = table_sort(filename)
+            print(sorted_df)
+
+            return [sorted_df, return_flag]
+    else:
+        print("Error:", FileNotFoundError)
+        return [df_empty, -1]
 
 # edit(editedit, 'DRIVER')
-
